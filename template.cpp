f7117fa1705fae8a35ca74d91f315ea68e04adb2
ncudnos <63144312+Ncudnos@users.noreply.github.com>
2022-07-23 16:34:53 +0700

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define MAX 1e5 + 5
+#define all(a) (a).begin(), (a).end()
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &&i : a)
+    {
+        cin >> i;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int tc;
+    cin >> tc;
+    for (int i = 1; i <= tc; i++)
+    {
+        cout << "Case " << i << ": ";
+        solve();
+    }
+}
